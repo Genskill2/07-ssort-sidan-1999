2084e6b7fefd00d82fbe69a27a9509f1ce6264b7
sidan-1999 <70771908+sidan-1999@users.noreply.github.com>
2021-07-02 23:59:55 +0530

    Create swap.c

---
+++ swap.c
@@ -0,0 +1,20 @@
+void swap_max(int arr[], int l, int n ){
+    int p,max;
+    max=arr[n];
+    for(int i=0; i<(l-n); ++i)
+    {
+		   if(max<=arr[i+n]){
+		        max=arr[i+n];
+		        p=i+n;
+		       
+		   }
+    }
+    
+  arr[p]=arr[n];
+  arr[n]=max;
+}
+void ssort(int arr[], int l){
+    for(int i=0;i<l-1;++i){
+        swap_max(arr, l, i);
+    }
+}
